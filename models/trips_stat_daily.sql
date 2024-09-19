ea80fc639c0e0de5f82c543af7729714a26bd0a9
Aliaksei Ihnatsyeu <ignatiev1551@mail.ru>
2024-09-19 08:45:16 +0000

    Creating tables: trips_stat, trips_stat_daily

---
+++ models/trips_stat_daily.sql
@@ -0,0 +1,11 @@
+select 
+  cast(started_at as date) as date,
+  count (*) as trips,
+  max(price)/100 as max_price_rub,
+  avg(distance)/1000 as avg_distance_km
+from 
+  scooters_raw.trips
+group by 
+  1
+order by 
+  1
--- target1/compiled/dbt_scooters/models/example/my_second_dbt_model.sql
+++
@@ -1,5 +0,0 @@
--- Use the `ref` function to select from other models
-
-select *
-from "dev_bw2n"."dbt"."my_first_dbt_model"
-where id = 1

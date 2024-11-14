1cd47191c521c1e6398e9fa01390dc104616aa4e
Aliaksei Ihnatsyeu <ignatiev1551@mail.ru>
2024-11-14 09:35:46 +0000

    Add tests on uniqueness and not null for model "age_group", add extra test for model "trips_age_daily" on uniqueness using the composite key

---
+++ tests/generic/unique_key.sql
@@ -0,0 +1,13 @@
+{% test unique_key(model, columns) %}
+select  
+  {% for column_name in columns %}
+    {% if not loop.last %}
+      "{{ column_name }}"::text ||
+    {% else %}
+      "{{ column_name }}"
+    {% endif %}
+  {% endfor %}
+from {{ model }}
+group by 1
+having count(*) > 1 
+{% endtest %}

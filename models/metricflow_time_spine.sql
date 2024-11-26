41c438d39408ac2204e6a9bd8ae5b36253068d62
Alexei Ignatiev <ignatiev1551@mail.ru>
2024-11-26 13:11:17 +0300

    Add the semantic model for the scooter entity

--- models/metricflow_time_spine.sql
+++ models/metricflow_time_spine.sql
@@ -1,12 +1,12 @@
-with days as (
+with hours as (
     {{ dbt.date_spine(
-        'day',
+        'hour',
         "date '2023-06-01'",
         "date '2023-08-31'") }}
 ),
 final as (
-    select cast(date_day as date) as date_day
-    from days
+    select cast(date_hour as timestamp) as date_hour
+    from hours
 )
 select *
 from final
